082255428696376b9f4963c5b7f0d94317bd81cd
yiranshu <shuyiran1425@hotmail.com>
2024-03-19 11:43:18 -0400

    function more

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    let satActivity = activity
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    let monActivity = activity
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrap='*'){
+    return function(adj='special'){
+        return`You are ${wrap}${adj}${wrap}!`
+    }
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
+encouragingPromptFunction ()
+wrapAdjective('!!')('cool')
